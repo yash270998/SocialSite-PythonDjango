d81f5d12d415fcc76d4dabee7d03675027b07ffd
yash270998 <a00268810@student.ait.ie>
2020-06-21 01:59:52 +0100

    Group Templates edited

--- socialsite/groups/views.py
+++ socialsite/groups/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 # Create your views here.
+from django.urls import reverse
+from django.views.generic import CreateView,DetailView, ListView
+from groups.models import Group, GroupMember
+class CreateGroup(LoginRequiredMixin,CreateView):
+    fields = ('name','description')
+    model = Group
+
+class SingleGroup(DetailView):
+    model = Group
+
+class ListGroups(ListView):
+    model = Group
+
+
